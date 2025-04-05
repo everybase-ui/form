2aa67f606cdebf6a53a6783df0908bb1f3e4f130
tdd28 <tdd2810@gmail.com>
2025-04-06 01:36:29 +0700

    chore: get value in array

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,58 @@
-type PathOf<T> = T extends object ? {
-    [K in keyof T]: `${Exclude<K, symbol>}${"" | (PathOf<T[K]> extends `${infer P}` ? `.${P}` : never)}`
-}[keyof T] : never
+type Key = string | number
+type ArrayKey = `[${number}]`
 
-type ValueOf<T, P extends PathOf<T>> = P extends `${infer K}.${infer P1}`
-    ? K extends keyof T
-        ?  P1 extends PathOf<T[K]>
-            ? ValueOf<T[K], P1>
+type JoinPath<P1 extends Key, P2 extends Key> = `${P1}${P2 extends `[${number}]${string}` ? P2 : `.${P2}`}`
+type DottedPath<P extends string, Root = true> = P extends `${infer P1}[${infer K}]${infer P3}`
+    ? K extends `${number}`
+        ? Root extends true
+            ? P1 extends ''
+                ? `[${K}]${DottedPath<P3, false>}`
+                : `${P1}.[${K}]${DottedPath<P3, false>}`
+            : `${P1}.[${K}]${DottedPath<P3, false>}`
+        : P
+    : P
+
+export type PathOf<T> = T extends object ? (
+    T extends Array<infer I> ? (
+        ArrayKey | JoinPath<ArrayKey, PathOf<I>>
+    ) : (
+        {
+            [K in keyof T]: K extends `${Key}`
+                ? K | JoinPath<K, PathOf<T[K]>>
+                : never
+        }[keyof T]
+    )
+) : never
+
+type ValueInDottedPath<T, P extends DottedPath<PathOf<T>>> = P extends `${infer K}.${infer P1}`
+    ? K extends `[${number}]`
+        ? T extends Array<infer I>
+            ? P1 extends DottedPath<PathOf<I>>
+                ? ValueInDottedPath<I, P1>
+                : never
             : never
-        : never
-    : P extends keyof T ? T[P] : never
+        : K extends keyof T
+            ? P1 extends DottedPath<PathOf<T[K]>>
+                ? ValueInDottedPath<T[K], P1>
+                : never
+            : never
+    : P extends `[${number}]`
+        ? T extends Array<infer I>
+            ? I
+            : never
+        : P extends keyof T
+            ? T[P]
+            : never
 
-export function getIn<T, P extends PathOf<T>>(object: T, path: P): ValueOf<T, P> {
-    return split(path).reduce<any>((result, key) => result[key], object)
+export type ValueIn<T, P extends PathOf<T>> = ValueInDottedPath<T, DottedPath<P>>
+
+const ARRAY_KEY_REGEX = /^\[(\d+)\]$/
+const NON_LEADING_ARRAY_KEY_REGEX = /(?!^)\[\d+\]/g
+
+export function getIn<T, P extends PathOf<T>>(object: T, path: P): ValueIn<T, P> {
+    return split(path).reduce<any>((result, key) => result[key.match(ARRAY_KEY_REGEX)?.[1] || key], object)
 }
 
 function split(path: string) {
-    return path.split('.')
+    return path.replace(NON_LEADING_ARRAY_KEY_REGEX, p => '.' + p).split('.')
 }
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,33 +1,92 @@
 import { getIn } from '../src/utils'
+import type { PathOf, ValueIn } from '../src/utils'
+
+const starships = [
+    {
+        name: "Naboo fighter",
+        length: 11,
+        cargo_capacity: 65
+    },
+    {
+        name: "Trade Federation cruiser",
+        length: 1088,
+        cargo_capacity: 50000000
+    },
+    {
+        name: 'Jedi Interceptor',
+        length: 5.47,
+        cargo_capacity: 60
+    }
+]
+
+const character = {
+    name: 'Anakin Skywalker',
+    height: 188,
+    wife: {
+        name: 'Padmé Amidala',
+        height: 185
+    },
+    starships
+}
+
+const characters = [character]
+
+type Starships = typeof starships
+type Character = typeof character
+type Characters = typeof characters
+
+describe('types', () => {
+    it('PathOf', () => {
+        const starshipsPaths: PathOf<Starships>[] = ['[0]', '[1].name', '[10].length', '[999].cargo_capacity']
+        expect(starshipsPaths).toBeDefined()
+
+        const characterPaths: PathOf<Character>[] = ['height', 'name', 'wife', 'wife.height', 'wife.name', 'starships', 'starships[0].name', 'starships[1].length', 'starships[10].cargo_capacity']
+        expect(characterPaths).toBeDefined()
+    })
+
+    it ('ValueIn', () => {
+        const starship: ValueIn<Starships, '[0]'> = starships[0]
+        expect(starship).toBeDefined()
+
+        const starshipName: ValueIn<Starships, '[0].name'> = starships[0].name
+        expect(starshipName).toBeDefined()
+
+        const starshipLength: ValueIn<Starships, '[1].length'> = starships[1].length
+        expect(starshipLength).toBeDefined()
+
+        const starshipCargoCapacity: ValueIn<Starships, '[2].cargo_capacity'> = starships[2].length
+        expect(starshipCargoCapacity).toBeDefined()
+
+        const characterName: ValueIn<Character, 'name'> = character.name
+        expect(characterName).toBeDefined()
+
+        const characterWifeHeight: ValueIn<Character, 'wife.height'> = character.wife.height
+        expect(characterWifeHeight).toBeDefined()
+
+        const characterStarshipName: ValueIn<Character, 'starships[0].name'> = character.starships[0].name
+        expect(characterStarshipName).toBeDefined()
+
+        const firstCharacterStarshipLength: ValueIn<Characters, '[0].starships[0].length'> = characters[0].starships[0].length
+        expect(firstCharacterStarshipLength).toBeDefined()
+    })
+})
 
 describe('getIn', () => {
-    const object = {
-        name: 'Anakin Skywalker',
-        height: 188,
-        wife: {
-            name: 'Padmé Amidala',
-            height: 185
-        },
-        starships: [
-            {
-                name: "Naboo fighter",
-                length: 11
-            },
-            {
-                name: "Trade Federation cruiser",
-                length: 1088
-            },
-            {
-                name: 'Jedi Interceptor',
-                length: 5.47
-            }
-        ]
-    }
-
     it('returns matched value against path', () => {
-        expect(getIn(object, 'name')).toBe('Anakin Skywalker')
-        expect(getIn(object, 'height')).toBe(188)
-        expect(getIn(object, 'wife.name')).toBe('Padmé Amidala')
-        expect(getIn(object, 'wife.height')).toBe(185)
+        expect(getIn(starships, '[1].name')).toBe('Trade Federation cruiser')
+        expect(getIn(starships, '[2].length')).toBe(5.47)
+        expect(getIn(starships, '[0].cargo_capacity')).toBe(65)
+
+        expect(getIn(character, 'name')).toBe('Anakin Skywalker')
+        expect(getIn(character, 'height')).toBe(188)
+        expect(getIn(character, 'wife.name')).toBe('Padmé Amidala')
+        expect(getIn(character, 'wife.height')).toBe(185)
+        expect(getIn(character, 'starships[0].name')).toBe('Naboo fighter')
+        expect(getIn(character, 'starships[1].length')).toBe(1088)
+        expect(getIn(character, 'starships[2].cargo_capacity')).toBe(60)
+
+        expect(getIn(characters, '[0].starships[2].name')).toBe('Jedi Interceptor')
+        expect(getIn(characters, '[0].starships[0].length')).toBe(11)
+        expect(getIn(characters, '[0].starships[1].cargo_capacity')).toBe(50000000)
     })
 })
