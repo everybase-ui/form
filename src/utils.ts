b22a59464c35ab2ef16c79d4dcb70884bc5c7e50
tdd28 <tdd2810@gmail.com>
2025-04-05 11:45:12 +0700

    chore: get value of nested object

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,19 @@
-type Path<T> = T extends object
-    ? keyof T
-    : never
+type PathOf<T> = T extends object ? {
+    [K in keyof T]: `${Exclude<K, symbol>}${"" | (PathOf<T[K]> extends `${infer P}` ? `.${P}` : never)}`
+}[keyof T] : never
 
-export function getIn<T, P extends Path<T>>(object: T, path: P) {
-    return object[path]
+type ValueOf<T, P extends PathOf<T>> = P extends `${infer K}.${infer P1}`
+    ? K extends keyof T
+        ?  P1 extends PathOf<T[K]>
+            ? ValueOf<T[K], P1>
+            : never
+        : never
+    : P extends keyof T ? T[P] : never
+
+export function getIn<T, P extends PathOf<T>>(object: T, path: P): ValueOf<T, P> {
+    return split(path).reduce<any>((result, key) => result[key], object)
+}
+
+function split(path: string) {
+    return path.split('.')
 }
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -2,22 +2,32 @@ import { getIn } from '../src/utils'
 
 describe('getIn', () => {
     const object = {
-        name: 'Luke Skywalker',
-        height: 172,
+        name: 'Anakin Skywalker',
+        height: 188,
+        wife: {
+            name: 'Padmé Amidala',
+            height: 185
+        },
         starships: [
             {
-                name: "X-wing",
-                length: 12.5
+                name: "Naboo fighter",
+                length: 11
             },
             {
-                name: "Imperial shuttle",
-                length: 20
+                name: "Trade Federation cruiser",
+                length: 1088
+            },
+            {
+                name: 'Jedi Interceptor',
+                length: 5.47
             }
         ]
     }
 
     it('returns matched value against path', () => {
-        expect(getIn(object, 'name')).toBe('Luke Skywalker')
-        expect(getIn(object, 'height')).toBe(172)
+        expect(getIn(object, 'name')).toBe('Anakin Skywalker')
+        expect(getIn(object, 'height')).toBe(188)
+        expect(getIn(object, 'wife.name')).toBe('Padmé Amidala')
+        expect(getIn(object, 'wife.height')).toBe(185)
     })
 })
